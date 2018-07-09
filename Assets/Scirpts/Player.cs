05daf3c115d1f0c95b3f816d81b0f4122bd76059
Brian Cain <bcain@hashicorp.com>
2018-07-09 12:49:30 -0700

    Add basic player gameobject

---
+++ Assets/Scirpts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+  private enum PlayerDirection {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
+  }
+
+  private float playerSpeed;
+
+  // Use this for initialization
+  void Start () {
+  }
+  // Update is called once per frame
+  void Update () {
+  }
+}
