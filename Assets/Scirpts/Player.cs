4f7cad9b6e69a1b9e2779aeecb17d032e6ea96da
Brian Cain <brianccain@gmail.com>
2018-07-09 21:59:11 -0700

    Set up initial scene and begin to add player activity

--- Assets/Scirpts/Player.cs
+++ Assets/Scirpts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+  private Animator am;
+  private Rigidbody2D rb;
+
   private enum PlayerDirection {
     NORTH,
     SOUTH,
@@ -11,12 +14,38 @@ public class Player : MonoBehaviour {
     WEST
   }
 
+  private PlayerDirection playerDir;
   private float playerSpeed;
+
+
+  void Awake() {
+    InitVars();
+  }
+
+  void InitVars() {
+    playerSpeed = 3f;
+    am = gameObject.GetComponent<Animator>();
+    rb = gameObject.GetComponent<Rigidbody2D>();
+  }
+
+  void SetPlayerDirection(PlayerDirection dir){
+    if (PlayerDirection.IsDefined(typeof(PlayerDirection), dir)) {
+      Debug.LogError("Given an invalid player direction: " + dir);
+    }
+
+    playerDir = dir;
+  }
+
+  void MovePlayer() {
+    Vector3 move;
+  }
 
   // Use this for initialization
   void Start () {
   }
   // Update is called once per frame
   void Update () {
+    MovePlayer();
   }
+
 }
